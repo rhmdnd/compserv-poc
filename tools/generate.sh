0538a423cfaa601931066afb3d6798d445c7142d
Lance Bragstad <lbragsta@redhat.com>
2022-05-09 16:13:54 -0500

    Ensure we generate code in the same place regardless of the generator

--- tools/generate.sh
+++ tools/generate.sh
@@ -7,7 +7,7 @@ display_description() {
 print_usage() {
     cmd=$(basename $0)
     echo "Usage: $cmd"
-    echo "    -g [string]             Generator name (optional)."
+    echo "    -g [string]             Generator name (optional). The default is go-server, but go-gin-server is acceptable."
     echo "    -i [string]             OpenAPI specification input file (optional)."
     echo "    -h                      Display this help message."
 }
@@ -36,9 +36,21 @@ GENERATOR_NAME=${GENERATOR:-go-server}
 GENERATOR_PATH=$HOME/openapi-generator-cli-6.0.0-beta.jar
 INPUT=${SPECIFICATION:-https://raw.githubusercontent.com/EasyDynamics/oscal-rest/develop/openapi.yaml}
 
+# The go-gin-server and go-server use different arguments for the source code
+# path. Let's make sure we use the right one depending on the generator.
+if [[ $GENERATOR_NAME == "go-server" ]]; then
+  CODE_PATH_PROPERTY="--additional-properties=sourceFolder=compserv"
+elif [[ $GENERATOR_NAME == "go-gin-server" ]]; then
+  CODE_PATH_PROPERTY="--additional-properties=apiPath=compserv"
+else
+  echo "Generator $GENERATOR isn't supported by this script."
+  print_usage
+  exit 1
+fi
+
 java -jar $GENERATOR_PATH generate \
   -g $GENERATOR_NAME \
   -i $INPUT \
   --package-name compserv \
-  --additional-properties=sourceFolder=compserv \
+  $CODE_PATH_PROPERTY \
   --output $OUTPUT_DIR
